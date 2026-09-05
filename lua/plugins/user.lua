89584a4c0fedba218201304901a8ec6f6180e284
R3D-ubuntu <ryuk24.gaming@gmail.com>
2026-09-05 18:26:12 +0200

    better

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -9,12 +9,12 @@ return {
 
   -- == Examples of Adding Plugins ==
 
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function() require("lsp_signature").setup() end,
+  -- },
 
   -- == Examples of Overriding Plugins ==
 
@@ -36,29 +36,29 @@ return {
             -- "██ ██  ██ ██    ██ ██ ██ ████ ██",
             -- "██  ██ ██  ██  ██  ██ ██  ██  ██",
             -- "██   ████   ████   ██ ██      ██",
-"            ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                                                  ",
-"              ▓▓▓▓▓███████████▓     ▓▓▓█▓███████████▓▓▓              ▒▒                                    ",
-"                ▓▓▓▓▓██████████▓       ▓█▓█▓██████▓▓               ▒▒░░▒▒                                  ",
-"                 ▓█▓▓▓▓▓███▓████▓      ▓█▓████▓██▓                ▒░▒░░▒▒                                  ",
-"                ▓▓▓▒▒▓▓▓▓▓▓█████▓     ▓█▓▓▓▓▓▓██▓                  ▓▒▒▒▓                                   ",
-"               ▓▓▓░▒▒▒▓▓▓▓▓▓█████▓   ▓█▓▓▓▓▓▓▓██▓▒▒▒▒▒    ▒▒▒▒▒▓▓          ▓▓▓▒▒▒▒▓▓  ▓▓▒▒▒▒▓    ▓▒▒▒▒▒    ",
-"               ▒▓░░▒▒▒▒▓▓▓▓▓▓█▓███▓ ▓▓▓▓▓▓▓▓▓██▓▒░░░░▓   ▒░░░░▒██▒░░░░░▒▓█▒▓░░░░░▒▓▓▒░░░░░░▒▒▓▓▒░░░░░░▒▒██ ",
-"              ▒▓░░░░░▒▒▒▓█▒▓▓▓▓███▓▓▓▓▓▓▓▓▓▓██▓▒░░░░▒   ▓░░░░▒██▓░░░░░▒▓██▒▒░░░░▒▒▒░░░░░░░▒▒▒▒░░░░░░░▒▒██▓▓",
-"             ▓▓▒░░░░░░▒▒█▓▓▒▓▓▓▓███▒█▓▓▓▓▓▓██▓▒░░░░▒    ░░░░▒▓██░░░░▒▒▒██▒▓░░░░░▒▓█▓▒▓░▒▒▒▒▒▓█▓▒▒░░▒▒▒▓██▓ ",
-"            ▓▒▓░░░░░░░░▓█▓▓▓▒▓▓▓▓▓██▓█▓█████▓▓░░░░░▓   ▒░░░░▓██▓░░░░▒▒███▒░░░░░▒█████▒░░▒▒▒████▓░░░▒▒▒██▓  ",
-"           ▓▒▓░░░░░░░░▓█▓ ▓█▒▒▒▒▓▓▓█████████▓░░░░░▓  ▒░░░░▒▓███▒░░░▒▒▓██▒▒░░░░▒▓██▓█▓░░░▒▒▓██▓█░░░░▒▒▓█▓▓  ",
-"           ░▓░░░░░░░░▒█▓  ▓▓▓▒▒▒▒▒▒▓▓██████▓░░░░░▒ ▒░░░░▒▓██▓█▒░░▒▒▒▒██▒▓░░░▒▒▒██▓█▓░░░▒▒▒██▓█▒░░▒▒▒▓██▓   ",
-"          ▒▓▒░░░░░░░▒█▓▓   ▓█▓▓▓▓▓▓▓▓▓▓▓██▓▒░░░░░▒░░░░▒▓██▓▓ ▓░░░░░▒██▒▓░░░░░▒▓████░░░░░▒██▓█▓░░░▒▒▒██▓▓   ",
-"         ▓▒▓░░░░░░░▒██▓     ▓█▓▓█▓▓██▓▓██▓▓░░░░░░░░░▒▓██▓▓▓  ░░░░░░▓██▒░░░░░░▓██▓█░░░░░░▓███▓░░░░░░▒██▓    ",
-"         ▓▓░░░░░░░▒▓█▓      ▓█▒▒▓▓▓█████▓ ▓▒▒▒▒▒▒▒▒▓██▓▓    ▓▒▒▒▒▒▒██▒▓▒▒▒▒▒▓██▓ ▓▒▒▒▒▒▓██▓ ▒▒▒▒▒▒▒▒▓▓▓    ",
-"        ▒▓░░░░░░░░▒█▓        ▓▓░░░░▒▒▓██▓   ▓▓▓▓▓▓█▓▓▓       ▓▓▓▓▓███▓ ▓▓▓▓▓█▓▓   ▓▓▓▓██▓▓   ▓▓▓▓▓███▓     ",
-"   ▓▓▓░░░░░░░░░░░▒▒▒▒▓        ▓▒░░░░▒██▓     ▓▓▓▓▓▓▓           ▓▓▓▓▓▓    ▓▓▓▓▓      ▓▓▓▓▓      ▓▓▓▓▓▓      ",
-"    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       ▓█▒░░░██▓                                                                    ",
-"      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       ▓▓░░▓█▓                                                                     ",
-"                                ▓█▓█▓                                                                      ",
-"                                ▓▓█▓▓                                                                      ",
-"                                 ▓▓▓                                                                       ",
-"                                  ▓                                                                        ",
+            "            ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                                                  ",
+            "              ▓▓▓▓▓███████████▓     ▓▓▓█▓███████████▓▓▓              ▒▒                                    ",
+            "                ▓▓▓▓▓██████████▓       ▓█▓█▓██████▓▓               ▒▒░░▒▒                                  ",
+            "                 ▓█▓▓▓▓▓███▓████▓      ▓█▓████▓██▓                ▒░▒░░▒▒                                  ",
+            "                ▓▓▓▒▒▓▓▓▓▓▓█████▓     ▓█▓▓▓▓▓▓██▓                  ▓▒▒▒▓                                   ",
+            "               ▓▓▓░▒▒▒▓▓▓▓▓▓█████▓   ▓█▓▓▓▓▓▓▓██▓▒▒▒▒▒    ▒▒▒▒▒▓▓          ▓▓▓▒▒▒▒▓▓  ▓▓▒▒▒▒▓    ▓▒▒▒▒▒    ",
+            "               ▒▓░░▒▒▒▒▓▓▓▓▓▓█▓███▓ ▓▓▓▓▓▓▓▓▓██▓▒░░░░▓   ▒░░░░▒██▒░░░░░▒▓█▒▓░░░░░▒▓▓▒░░░░░░▒▒▓▓▒░░░░░░▒▒██ ",
+            "              ▒▓░░░░░▒▒▒▓█▒▓▓▓▓███▓▓▓▓▓▓▓▓▓▓██▓▒░░░░▒   ▓░░░░▒██▓░░░░░▒▓██▒▒░░░░▒▒▒░░░░░░░▒▒▒▒░░░░░░░▒▒██▓▓",
+            "             ▓▓▒░░░░░░▒▒█▓▓▒▓▓▓▓███▒█▓▓▓▓▓▓██▓▒░░░░▒    ░░░░▒▓██░░░░▒▒▒██▒▓░░░░░▒▓█▓▒▓░▒▒▒▒▒▓█▓▒▒░░▒▒▒▓██▓ ",
+            "            ▓▒▓░░░░░░░░▓█▓▓▓▒▓▓▓▓▓██▓█▓█████▓▓░░░░░▓   ▒░░░░▓██▓░░░░▒▒███▒░░░░░▒█████▒░░▒▒▒████▓░░░▒▒▒██▓  ",
+            "           ▓▒▓░░░░░░░░▓█▓ ▓█▒▒▒▒▓▓▓█████████▓░░░░░▓  ▒░░░░▒▓███▒░░░▒▒▓██▒▒░░░░▒▓██▓█▓░░░▒▒▓██▓█░░░░▒▒▓█▓▓  ",
+            "           ░▓░░░░░░░░▒█▓  ▓▓▓▒▒▒▒▒▒▓▓██████▓░░░░░▒ ▒░░░░▒▓██▓█▒░░▒▒▒▒██▒▓░░░▒▒▒██▓█▓░░░▒▒▒██▓█▒░░▒▒▒▓██▓   ",
+            "          ▒▓▒░░░░░░░▒█▓▓   ▓█▓▓▓▓▓▓▓▓▓▓▓██▓▒░░░░░▒░░░░▒▓██▓▓ ▓░░░░░▒██▒▓░░░░░▒▓████░░░░░▒██▓█▓░░░▒▒▒██▓▓   ",
+            "         ▓▒▓░░░░░░░▒██▓     ▓█▓▓█▓▓██▓▓██▓▓░░░░░░░░░▒▓██▓▓▓  ░░░░░░▓██▒░░░░░░▓██▓█░░░░░░▓███▓░░░░░░▒██▓    ",
+            "         ▓▓░░░░░░░▒▓█▓      ▓█▒▒▓▓▓█████▓ ▓▒▒▒▒▒▒▒▒▓██▓▓    ▓▒▒▒▒▒▒██▒▓▒▒▒▒▒▓██▓ ▓▒▒▒▒▒▓██▓ ▒▒▒▒▒▒▒▒▓▓▓    ",
+            "        ▒▓░░░░░░░░▒█▓        ▓▓░░░░▒▒▓██▓   ▓▓▓▓▓▓█▓▓▓       ▓▓▓▓▓███▓ ▓▓▓▓▓█▓▓   ▓▓▓▓██▓▓   ▓▓▓▓▓███▓     ",
+            "   ▓▓▓░░░░░░░░░░░▒▒▒▒▓        ▓▒░░░░▒██▓     ▓▓▓▓▓▓▓           ▓▓▓▓▓▓    ▓▓▓▓▓      ▓▓▓▓▓      ▓▓▓▓▓▓      ",
+            "    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       ▓█▒░░░██▓                                                                    ",
+            "      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       ▓▓░░▓█▓                                                                     ",
+            "                                ▓█▓█▓                                                                      ",
+            "                                ▓▓█▓▓                                                                      ",
+            "                                 ▓▓▓                                                                       ",
+            "                                  ▓                                                                        ",
           }, "\n"),
         },
       },
@@ -70,98 +70,99 @@ return {
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
+    --   "L3MON4D3/LuaSnip",
+    --   config = function(plugin, opts)
+      --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      --     -- add more custom luasnip configuration such as filetype extend or custom snippets
+      --     local luasnip = require "luasnip"
+      --     luasnip.filetype_extend("javascript", { "javascriptreact" })
+      --   end,
+      -- },
 
-  -- {
-  --   "windwp/nvim-autopairs",
-  --   config = function(plugin, opts)
-  --     require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom autopairs configuration such as custom rules
-  --     local npairs = require "nvim-autopairs"
-  --     local Rule = require "nvim-autopairs.rule"
-  --     local cond = require "nvim-autopairs.conds"
-  --     npairs.add_rules(
-  --       {
-  --         Rule("$", "$", { "tex", "latex" })
-  --           -- don't add a pair if the next character is %
-  --           :with_pair(cond.not_after_regex "%%")
-  --           -- don't add a pair if  the previous character is xxx
-  --           :with_pair(
-  --             cond.not_before_regex("xxx", 3)
-  --           )
-  --           -- don't move right when repeat character
-  --           :with_move(cond.none())
-  --           -- don't delete if the next character is xx
-  --           :with_del(cond.not_after_regex "xx")
-  --           -- disable adding a newline when you press <cr>
-  --           :with_cr(cond.none()),
-  --       },
-  --       -- disable for .vim files, but it work for another filetypes
-  --       Rule("a", "a", "-vim")
-  --     )
-  --   end,
-  -- },
-  -- { "catppuccin/nvim", name = "catppuccin" },
-  { "folke/tokyonight.nvim" },
-  -- {
-  --   "nvim-tree/nvim-tree.lua",
-  --   dependencies = "nvim-tree/nvim-web-devicons", -- optional, for icons
-  --   config = function()
-  --     require("nvim-tree").setup({
-  --       view = {
-  --         width = 30,
-  --         side = "left",
-  --       },
-  --       update_focused_file = {
-  --         enable = true,
-  --       },
-  --     })
-  --
-  --     -- optional keybinding
-  --     vim.keymap.set('n', '<leader>E', ':NvimTreeToggle<CR>', { silent = true })
-  --   end,
-  -- },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-
-  -- Telescope file browser extension
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    config = function()
-      require("telescope").load_extension("file_browser")
-    end,
-  },
-  {
-    "kevinhwang91/nvim-hlslens",
-    config = function()
-      require("hlslens").setup()
-    end
-  },{
-  "eldritch-theme/eldritch.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  },
-  {
-    "dgox16/oldworld.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-  "thesimonho/kanagawa-paper.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  },
-}
+      -- {
+        --   "windwp/nvim-autopairs",
+        --   config = function(plugin, opts)
+          --     require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+          --     -- add more custom autopairs configuration such as custom rules
+          --     local npairs = require "nvim-autopairs"
+          --     local Rule = require "nvim-autopairs.rule"
+          --     local cond = require "nvim-autopairs.conds"
+          --     npairs.add_rules(
+            --       {
+              --         Rule("$", "$", { "tex", "latex" })
+              --           -- don't add a pair if the next character is %
+              --           :with_pair(cond.not_after_regex "%%")
+              --           -- don't add a pair if  the previous character is xxx
+              --           :with_pair(
+                --             cond.not_before_regex("xxx", 3)
+                --           )
+                --           -- don't move right when repeat character
+                --           :with_move(cond.none())
+                --           -- don't delete if the next character is xx
+                --           :with_del(cond.not_after_regex "xx")
+                --           -- disable adding a newline when you press <cr>
+                --           :with_cr(cond.none()),
+                --       },
+                --       -- disable for .vim files, but it work for another filetypes
+                --       Rule("a", "a", "-vim")
+                --     )
+                --   end,
+                -- },
+                -- { "catppuccin/nvim", name = "catppuccin" },
+                { "folke/tokyonight.nvim" },
+                -- {
+                  --   "nvim-tree/nvim-tree.lua",
+                  --   dependencies = "nvim-tree/nvim-web-devicons", -- optional, for icons
+                  --   config = function()
+                    --     require("nvim-tree").setup({
+                      --       view = {
+                        --         width = 30,
+                        --         side = "left",
+                        --       },
+                        --       update_focused_file = {
+                          --         enable = true,
+                          --       },
+                          --     })
+                          --
+                          --     -- optional keybinding
+                          --     vim.keymap.set('n', '<leader>E', ':NvimTreeToggle<CR>', { silent = true })
+                          --   end,
+                          -- },
+                          -- {
+                          --   "nvim-telescope/telescope.nvim",
+                          --   dependencies = { "nvim-lua/plenary.nvim" },
+                          -- },
+                          --
+                          -- -- Telescope file browser extension
+                          -- {
+                          --   "nvim-telescope/telescope-file-browser.nvim",
+                          --   dependencies = { "nvim-telescope/telescope.nvim" },
+                          --   config = function()
+                          --     require("telescope").load_extension("file_browser")
+                          --   end,
+                          -- },
+                          {
+                            "kevinhwang91/nvim-hlslens",
+                            config = function()
+                              require("hlslens").setup()
+                            end
+                          },
+                          -- {
+                          --   "eldritch-theme/eldritch.nvim",
+                          --   lazy = false,
+                          --   priority = 1000,
+                          --   opts = {},
+                          -- },
+                          -- {
+                          --   "dgox16/oldworld.nvim",
+                          --   lazy = false,
+                          --   priority = 1000,
+                          -- },
+                          -- {
+                          --   "thesimonho/kanagawa-paper.nvim",
+                          --   lazy = false,
+                          --   priority = 1000,
+                          --   opts = {},
+                          -- },
+                        }
 
